8d1ae27aa7afce04f3df617da8f99208ca3d17f9
aniket <aniket2contact@gmail.com>
2023-10-06 22:57:25 +0530

    Fixed Basket Issue

--- basket/basket.py
+++ basket/basket.py
@@ -12,13 +12,21 @@ class Basket():
             basket = self.session['skey'] = {}
         self.basket = basket
 
-    def add(self, product):
+    def add(self, product, qty):
         """
         Adding and updating the users basket session data
         """
-        product_id = product.id 
+        product_id = product.id
 
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': product.price}
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
 
         self.session.modified = True
+
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+
+        return sum(item['qty'] for item in self.basket.values())
--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.product_all, name='product_all'),
-    path('<slug:slug>/', views.product_detail, name='product_detail'),
+    path('<slug:slug>', views.product_detail, name='product_detail'),
     path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
 
 ]
